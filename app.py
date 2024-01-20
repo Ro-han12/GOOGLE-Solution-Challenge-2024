9f4d19ee3d2b6a4c8e750ff412518e99236a2441
rohith4088 <2109486@reva.edu.in>
2024-01-20 10:44:14 +0530

    minor change

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask , render_template,app
+from flask import Flask , render_template,app,request
 import pandas as pd
 import numpy as np
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
-
-
+# @app.route("/predict",methods = ['POST'])
+# def predict():
+#     data = request.json['json']
+#     print(data)
 if __name__ == "__main__":
     app.run(debug = True)
